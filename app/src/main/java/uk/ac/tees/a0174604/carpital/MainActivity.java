b1bab5bc27da89727dacfcf3eb4ddd2638d6d1a4
Boluwatife <boluyebisi@gmail.com>
2022-10-18 10:38:42 +0100

    first commit

---
+++ app/src/main/java/uk/ac/tees/a0174604/carpital/MainActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.a0174604.carpital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
