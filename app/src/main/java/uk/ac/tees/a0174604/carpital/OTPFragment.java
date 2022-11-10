ad8489c408a13f74f567a712603f1a22b9541cd9
Boluwatife <boluyebisi@gmail.com>
2022-11-10 03:08:36 +0000

    created otp verification material design

---
+++ app/src/main/java/uk/ac/tees/a0174604/carpital/OTPFragment.java
@@ -0,0 +1,24 @@
+package uk.ac.tees.a0174604.carpital;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OTPFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OTPFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_o_t_p, container, false);
+    }
+}
