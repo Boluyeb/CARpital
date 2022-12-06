5423840780f7cf0bd2317c3b8f766853d1ed6678
Boluwatife <boluyebisi@gmail.com>
2022-12-06 03:36:02 +0000

    Added google map api

--- app/src/main/java/uk/ac/tees/a0174604/carpital/SearchFragment.java
+++ app/src/main/java/uk/ac/tees/a0174604/carpital/SearchFragment.java
@@ -1,29 +1,110 @@
 package uk.ac.tees.a0174604.carpital;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 
+import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SearchFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends SupportMapFragment implements OnMapReadyCallback {
 
     private static SearchFragment unique;
+    private boolean isPermissionGranted;
+//    private MapView mapView;
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        super.onCreateView(inflater, container, savedInstanceState);
+        // Inflate the layout for this fragment
+//        super.onCreateView(inflater, container, savedInstanceState);
+//              View rootView =  inflater.inflate(R.layout.fragment_search, container, false);
+
+//              mapView = rootView.findViewById(R.id.map_view);
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-              View rootView =  inflater.inflate(R.layout.fragment_search, container, false);
-             return rootView;
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        checkMyPermission();
+
+
+
+        if(isPermissionGranted){
+//                  mapView.getMapAsync(this);
+//                  mapView.onCreate(savedInstanceState);
+            try{
+                SupportMapFragment supportMapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.frame_container);
+                supportMapFragment.getMapAsync(this);
+            }
+            catch (NullPointerException e){
+                e.printStackTrace();
+            }
+
+//
+        }
+
+//        return rootView;
     }
+
+
+
+//    }
+
+    private void checkMyPermission() {
+
+        Dexter.withContext(getActivity()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                isPermissionGranted =  true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getActivity().getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.cancelPermissionRequest();
+            }
+        }).check();
+    }
+
 
     //    use singleton pattern to instantiate the fragment
     public static SearchFragment newInstance(){
@@ -32,4 +113,53 @@ public class SearchFragment extends Fragment {
         }
         return unique;
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
+//    override methods for map view
+
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mapView.onStart();
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        mapView.onResume();
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        mapView.onPause();
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        mapView.onStop();
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        mapView.onDestroy();
+//    }
+////
+//    @Override
+//    public void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//    }
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mapView.onLowMemory();
+//    }
 }
